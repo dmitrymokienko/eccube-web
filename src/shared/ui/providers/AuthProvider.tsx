6b4ecc57f80daf1d661b0f15d981c1d48f0d92cc
Dmitrii Mokienko <d.mokienko@maroo.us>
2024-01-08 11:23:58 +0400

    feat: refresh token

--- src/shared/ui/providers/AuthProvider.tsx
+++ src/shared/ui/providers/AuthProvider.tsx
@@ -1,9 +1,15 @@
 import { ReactNode, createContext, useCallback, useEffect, useState } from "react";
 import { currentUser } from "../../../entities/currentUser/model";
-import { checkLoggedInUserApi } from "../../../entities/auth/api";
+import { checkLoggedInUserApi, refreshTokenApi } from "../../../entities/auth/api";
 import { useUnit } from "effector-react";
 import { Nullable } from "../../types";
 import { IUser } from "../../../entities/currentUser/types";
+
+export interface IBackendTokens {
+    accessToken: string;
+    refreshToken: string;
+    expiresIn: number;
+}
 
 const defaultAuthContext: {
     userInfo: Nullable<Partial<IUser>>;
@@ -27,24 +33,52 @@ export function AuthProvider({ children }: IAuthProviderProps) {
     const userInfo = useUnit(currentUser.$info);
 
     const [loggedIn, setLoggedIn] = useState<Nullable<boolean>>(null);
+    const [tokens, setTokens] = useState<Nullable<IBackendTokens>>(null);
 
     const checkLoginState = useCallback(async () => {
         try {
-            const { user } = await checkLoggedInUserApi();
+            const { user, backendTokens } = await checkLoggedInUserApi();
             setLoggedIn(true);
-            if (user) {
+            if (user && backendTokens) {
                 currentUser.setInfo(user);
+                setTokens(backendTokens);
             }
         } catch (err) {
             console.error(err);
             setLoggedIn(false);
             currentUser.setInfo(null);
+            setTokens(null);
         }
     }, []);
+
+    const refresh = async () => {
+        try {
+            const { expiresIn, refreshToken } = tokens || {};
+            if (!expiresIn || !refreshToken) {
+                throw new Error("No tokens");
+            }
+            if (new Date().getTime() < expiresIn) {
+                return;
+            }
+            const res = await refreshTokenApi(refreshToken);
+            setTokens(res);
+        } catch (err) {
+            console.error(err);
+            setLoggedIn(false);
+            currentUser.setInfo(null);
+            setTokens(null);
+        }
+    };
 
     useEffect(() => {
         checkLoginState();
     }, [checkLoginState]);
+
+    // check it every minute
+    useEffect(() => {
+        const interval = setInterval(refresh, 60 * 1000);
+        return () => clearInterval(interval);
+    });
 
     return <AuthContext.Provider value={{ loggedIn, checkLoginState, userInfo }}>{children}</AuthContext.Provider>;
 }
--- vite.config.ts
+++ vite.config.ts
@@ -8,10 +8,8 @@ import fs from "fs";
 export default defineConfig({
     server: {
         https: {
-            // key: fs.readFileSync("./certificates/key.pem"),
-            // cert: fs.readFileSync("./certificates/cert.pem"),
-            key: fs.readFileSync("./certificates/bidding.eccube.key"),
-            cert: fs.readFileSync("./certificates/bidding.eccube.crt"),
+            key: fs.readFileSync("./src/core/ssl/bidding.eccube.key"),
+            cert: fs.readFileSync("./src/core/ssl/bidding.eccube.crt"),
         },
         host: "bidding.eccube.de",
         port: 443,
@@ -20,36 +18,5 @@ export default defineConfig({
         react(),
         svgr(),
         // basicSsl(), // dev only !!!
-
-        // svgr({
-        //     svgrOptions: {
-        //         plugins: ["@svgr/plugin-svgo", "@svgr/plugin-jsx"],
-        //         svgoConfig: {
-        //             plugins: [
-        //                 {
-        //                     name: "preset-default",
-        //                     params: {
-        //                         overrides: {
-        //                             /*
-        //                              * Prevent removing "viewBox" attribute to allow resizing images
-        //                              * via CSS `height` and `width` properties.
-        //                              */
-        //                             removeViewBox: false,
-        //                             /*
-        //                              * Prevent prefixing class names to allow SVG customization
-        //                              * via CSS.
-        //                              */
-        //                             // prefixClassNames: false,
-        //                         },
-        //                     },
-        //                 },
-        //                 /**
-        //                  * Enable IDs prefixing to avoid collisions across multiple SVGs.
-        //                  */
-        //                 "prefixIds",
-        //             ],
-        //         },
-        //     },
-        // }),
     ],
 });
