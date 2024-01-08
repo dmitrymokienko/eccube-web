30b94c36520f250b240ff29e853660547e86d21b
Dmitrii Mokienko <d.mokienko@maroo.us>
2024-01-08 20:25:43 +0400

    feat: refresh token

--- src/shared/ui/providers/AuthProvider.tsx
+++ src/shared/ui/providers/AuthProvider.tsx
@@ -4,6 +4,10 @@ import { checkLoggedInUserApi, refreshTokenApi } from "../../../entities/auth/ap
 import { useUnit } from "effector-react";
 import { Nullable } from "../../types";
 import { IUser } from "../../../entities/currentUser/types";
+import { createEvent, createStore } from "effector";
+import persist from "effector-localstorage";
+import { jwtDecode } from "jwt-decode";
+import { useInterval } from "../../hooks/useInterval";
 
 export interface IBackendTokens {
     accessToken: string;
@@ -23,6 +27,24 @@ const defaultAuthContext: {
     },
 };
 
+const INTERVAL = 60 * 1000; // 1 minute
+
+// EFFECTOR
+// TODO on the backend: generate new refresh-token via `checkLoggedInUserApi()` request
+// to avoid store sensitive data in localStorage
+// think about this solution or find better one
+
+// refresh token
+const setRefreshToken = createEvent<Nullable<string>>();
+const $refreshToken = createStore<Nullable<string>>(null);
+$refreshToken.on(setRefreshToken, (_, payload) => payload);
+persist({ key: "refresh", store: $refreshToken });
+// expires in
+const setExpiresIn = createEvent<Nullable<number>>();
+const $tokenExpiresIn = createStore<Nullable<number>>(null);
+$tokenExpiresIn.on(setExpiresIn, (_, payload) => payload);
+persist({ key: "expires", store: $tokenExpiresIn });
+
 export const AuthContext = createContext(defaultAuthContext);
 
 export interface IAuthProviderProps {
@@ -31,9 +53,10 @@ export interface IAuthProviderProps {
 
 export function AuthProvider({ children }: IAuthProviderProps) {
     const userInfo = useUnit(currentUser.$info);
+    const refreshToken = useUnit($refreshToken);
+    const expiresIn = useUnit($tokenExpiresIn);
 
     const [loggedIn, setLoggedIn] = useState<Nullable<boolean>>(null);
-    const [tokens, setTokens] = useState<Nullable<IBackendTokens>>(null);
 
     const checkLoginState = useCallback(async () => {
         try {
@@ -41,32 +64,37 @@ export function AuthProvider({ children }: IAuthProviderProps) {
             setLoggedIn(true);
             if (user && backendTokens) {
                 currentUser.setInfo(user);
-                setTokens(backendTokens);
+                setRefreshToken(backendTokens.refreshToken);
+                const { exp } = jwtDecode(backendTokens.accessToken);
+                if (exp) setExpiresIn(exp);
             }
         } catch (err) {
             console.error(err);
             setLoggedIn(false);
             currentUser.setInfo(null);
-            setTokens(null);
+            setRefreshToken(null);
+            setExpiresIn(null);
         }
     }, []);
 
     const refresh = async () => {
         try {
-            const { expiresIn, refreshToken } = tokens || {};
             if (!expiresIn || !refreshToken) {
                 throw new Error("No tokens");
             }
-            if (new Date().getTime() < expiresIn) {
+            if (new Date().getTime() < expiresIn * 1000) {
                 return;
             }
             const res = await refreshTokenApi(refreshToken);
-            setTokens(res);
+            setRefreshToken(res.refreshToken);
+            const { exp } = jwtDecode(res.accessToken);
+            if (exp) setExpiresIn(exp);
         } catch (err) {
             console.error(err);
             setLoggedIn(false);
             currentUser.setInfo(null);
-            setTokens(null);
+            setRefreshToken(null);
+            setExpiresIn(null);
         }
     };
 
@@ -74,11 +102,11 @@ export function AuthProvider({ children }: IAuthProviderProps) {
         checkLoginState();
     }, [checkLoginState]);
 
-    // check it every minute
-    useEffect(() => {
-        const interval = setInterval(refresh, 60 * 1000);
-        return () => clearInterval(interval);
-    });
+    // check access-token's expiration every minute
+    useInterval(() => {
+        if (!loggedIn) return;
+        refresh();
+    }, INTERVAL);
 
     return <AuthContext.Provider value={{ loggedIn, checkLoginState, userInfo }}>{children}</AuthContext.Provider>;
 }
