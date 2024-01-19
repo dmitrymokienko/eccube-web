859c34ec12dfa7d4a56c34af1c02120e78fa8896
Dmitrii Mokienko <d.mokienko@maroo.us>
2024-01-19 09:36:47 +0400

    feat: update secure - no localstorage sensetive data

--- src/entities/auth/model/index.ts
+++ src/entities/auth/model/index.ts
@@ -1,6 +1,6 @@
 import { combine, createEffect, createEvent, createStore } from 'effector'
 import { activateUserApi, loginUserApi, registerUserApi } from '../api'
-import persist from 'effector-localstorage'
+// import persist from 'effector-localstorage'
 import { Nullable } from '../../../shared/types'
 
 const registerFx = createEffect(registerUserApi)
@@ -18,12 +18,13 @@ const activateFx = createEffect(activateUserApi)
 const setRefreshToken = createEvent<Nullable<string>>()
 const $refreshToken = createStore<Nullable<string>>(null)
 $refreshToken.on(setRefreshToken, (_, payload) => payload)
-persist({ key: 'refresh', store: $refreshToken })
+// persist({ key: 'refresh', store: $refreshToken })
+
 // expires in
 const setExpiresIn = createEvent<Nullable<number>>()
 const $tokenExpiresIn = createStore<Nullable<number>>(null)
 $tokenExpiresIn.on(setExpiresIn, (_, payload) => payload)
-persist({ key: 'expires', store: $tokenExpiresIn })
+// persist({ key: 'expires', store: $tokenExpiresIn })
 
 const $isLoading = combine(
   registerFx.pending,
