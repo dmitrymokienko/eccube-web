4835efc123d01b979cb2ceefdec0af29470894fe
Dmitrii Mokienko <d.mokienko@maroo.us>
2024-05-21 13:54:12 +0400

    feat: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,27 +6,25 @@ import { fileURLToPath } from 'node:url'
 // import fs from "fs";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  // TODO: revert to https when SSL cert is available
-  server: {
-    // https: {
-    //     key: fs.readFileSync("./src/core/ssl/key.pem"),
-    //     cert: fs.readFileSync("./src/core/ssl/cert.pem"),
-    // },
-    host: 'bidding.eccube.de',
-    port: 443,
-  },
-  plugins: [
-    react(),
-    svgr(),
-    basicSsl(), // dev only !!!
-  ],
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: fileURLToPath(new URL('./src', import.meta.url)),
-      },
-    ],
-  },
+export default defineConfig((c) => {
+  return {
+    // TODO: revert to https when SSL cert is available
+    server: {
+      // https: {
+      //     key: fs.readFileSync("./src/core/ssl/key.pem"),
+      //     cert: fs.readFileSync("./src/core/ssl/cert.pem"),
+      // },
+      host: c.mode === 'development' ? 'bidding.eccube.de' : undefined,
+      port: c.mode === 'development' ? 443 : undefined,
+    },
+    plugins: [react(), svgr(), ...(c.mode === 'development' ? [basicSsl()] : [])],
+    resolve: {
+      alias: [
+        {
+          find: '@',
+          replacement: fileURLToPath(new URL('./src', import.meta.url)),
+        },
+      ],
+    },
+  }
 })
