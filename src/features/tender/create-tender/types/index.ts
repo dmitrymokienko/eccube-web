a7941a43a4ab340104265ec59c49728019296650
Dmitrii Mokienko <d.mokienko@maroo.us>
2024-08-19 12:31:22 +0400

    feat: support draft.js for tender

--- src/features/tender/create-tender/types/index.ts
+++ src/features/tender/create-tender/types/index.ts
@@ -1,7 +1,10 @@
+import { EditorState } from 'draft-js'
 import { prepareCreateTenderDtoMapper } from '../utils/mappers'
 import { ITender } from '@/entities/tender/types'
 
-export type CreatePlainTenderProcessForm = Omit<ITender, 'id'>
+export type CreatePlainTenderProcessForm = Omit<ITender, 'id' | 'workDescription'> & {
+  workDescription: EditorState
+}
 
 export type CreateTenderDto = ReturnType<typeof prepareCreateTenderDtoMapper>
 
